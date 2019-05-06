5f3bdc40e956da6bf8ee0bd1a64389df87f23473
killaadhy <40359280+killaadhy@users.noreply.github.com>
2019-05-06 12:50:20 +0200

    Add JUnit and first test

---
+++ Fowler/src/CustomerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+    @org.junit.Test
+    public void statement() {
+        String result;
+        Movie m1 = new Movie("movie1", 1);
+        Movie m2 = new Movie("movie2", 2);
+        Rental r1 = new Rental(m1, 10);
+        Rental r2 = new Rental(m2, 5);
+        Customer c1 = new Customer("joe");
+        c1.addRental(r1);   c1.addRental(r2);
+        System.out.println("Let's get the Statement");
+        result = c1.statement();
+        String expected ="Rental Record for joe\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tmovie1\t\t10\t30.0\n" +
+                "\tmovie2\t\t5\t4.5\n" +
+                "Amount owed is 34.5\n" +
+                "You earned 3 frequent renter points";
+        assertEquals(expected, result);
+
+     }
+
+}
