9ded33d6c909a898e3abaab83f9bf5cd9fe3405d
killaadhy <40359280+killaadhy@users.noreply.github.com>
2019-05-06 13:46:53 +0200

    Implementated htmlStatement

--- Fowler/src/Customer.java
+++ Fowler/src/Customer.java
@@ -82,6 +82,16 @@ class Customer {
         }
         return result;
     }*/	
-
+	public String htmlStatement() {
+		Enumeration rentals = this.rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H!><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+		return result;
+	}
 }
     
